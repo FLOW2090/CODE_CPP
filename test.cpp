aebed8df03a7f45348b2ca82a37e014d4917b5de
FLOW2090 <l-zh22@mails.tsinghua.edu.cn>
2023-05-23 16:57:57 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,12 +1,20 @@
-#include <algorithm>
 #include <iostream>
-#include <functional>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int fun1(int a){};
-int fun2(int a){};
+int main()
+{
+    vector<int> vec = {1, 2, 3, 4, 1, 2, 3, 4};
+    auto begin = vec.begin();
+    auto end = vec.end();
+    cout << *begin << ' ' << *end; // (1)
+    auto b = find(vec.begin(), vec.end(), 3);
 
-int main(){
-    function<int(int)> arr[] = {fun1,fun2};
-    cout << greater<int>()(2,1);
+    vec.erase(b + 1);         // (2)
+    vec.erase(vec.end() - 4); // (3)
+
+    cout << *end; // (4)
+
+    return 0;
 }
