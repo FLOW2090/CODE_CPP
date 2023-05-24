4b7db988226e2c5c04dd5333f05d38998c55dfd8
FLOW2090 <l-zh22@mails.tsinghua.edu.cn>
2023-05-24 23:48:20 +0800

    test

---
+++ OOP/HW6/HW6.2/myqueriable.h
@@ -0,0 +1,57 @@
+#include <vector>
+#include <functional>
+
+using std::vector;
+using std::function;
+
+template <class T>
+class MyQueriable
+{
+    private:
+        vector<T> element;
+    public:
+        MyQueriable() = default;
+        ~MyQueriable() = default;
+        friend MyQueriable<T>& from(const vector<T>& vec);
+        int size(){
+            return element.size();
+        }
+        T& operator [] (int i){
+            return element[i];
+        }
+        MyQueriable<T>& where(function<bool(T)> f) const{
+            vector<T> tmp;
+            for(auto &x : element)
+            {
+                if(f(x))
+                {
+                    tmp.push_back(x);
+                }
+            }
+            element = tmp;
+            return *this;
+        }
+        MyQueriable<T>& apply(function<T(T)> f) const{
+            for(auto &x : element)
+            {
+                x = f(x);
+            }
+            return *this;
+        }
+        T sum() const{
+            T _sum = 0;
+            for(int i = 0;i < element.size();i++)
+            {
+                _sum += element[i];
+            }
+            return _sum;
+        }
+};
+
+template <class T>
+MyQueriable<T>& from(const vector<T>& vec)
+{
+    MyQueriable<T> tmp;
+    tmp.element = vec;
+    return tmp;
+}
---
+++ OOP/HW6/HW6.4/sort.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include "compare.h"
+#include "sortlevel.h"
+using namespace std;
+
+Compare<vector<int>> make_comp(list<sort_level> sortlevels)
+{
+    // todo
+}
