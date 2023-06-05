cea8079c502e2098fb256780dfa0799ccade2917
FLOW2090 <l-zh22@mails.tsinghua.edu.cn>
2023-06-05 09:35:56 +0800

    HW6.1

---
+++ OOP/HW6/HW6.4/compare.h
@@ -0,0 +1,11 @@
+#pragma once
+using namespace std;
+
+template <class T>
+class Compare
+{
+    private:
+        T p;
+    public:
+
+};
--- test.cpp
+++ test.cpp
@@ -1,31 +1,11 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-namespace haha{
-    void func(){;}
-};
 
-namespace huhu{
-    void func(){;}
-};
-
-using std::vector;
-using std::cout;
-using namespace haha;
-using namespace huhu;
+using namespace std;
 
 int main()
 {
-    vector<int> vec = {1, 2, 3, 4, 1, 2, 3, 4};
-    auto begin = vec.begin();
-    auto end = vec.end();
-    cout << *begin << ' ' << *end; // (1)
-    auto b = find(vec.begin(), vec.end(), 3);
-
-    vec.erase(b + 1);         // (2)
-    vec.erase(vec.end() - 4); // (3)
-
-    cout << *end; // (4)
-
+    vector<int> vec(10);
+    vec.push_back(5);
     return 0;
 }
